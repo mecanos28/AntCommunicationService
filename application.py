22829e2c6382ee21b6376c51c3f092bacd96b1b0
mecanos28 <ferojasmel@hotmail.com>
2023-08-08 18:25:47 -0600

    Consulta periódica a entorno (#1)
    Agrega consulta periódica a entorno
    
    ---------
    
    Co-authored-by: Fernando Rojas <ferdorom@amazon.com>

--- application.py
+++ application.py
@@ -1,41 +1,101 @@
-from flask import Flask, request, app
-# import boto3
-# import requests
-# import json
-# import time
+import logging
+from apscheduler.schedulers.background import BackgroundScheduler
+from flask import Flask, request
+import boto3
+import requests
+import uuid
+from datetime import datetime
 
+### Constantes ###
+POLL_TO_ENTORNO_SECONDS = 10  # Actualmente configurado a 10 segundos para pruebas. Debe ser 60 segundos en producción.
+ENTORNO_NEXT_ELEMENT_URL = "http://placeholder.url/api/environment/next"  # Reemplazar con la URL real cuando esté disponible
+
+### Configuración de Registro (Logging) ###
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger()
+
+### Configuración del Programador (Scheduler) ###
+def tarea_programada():
+    """Función para acceder al endpoint poll_entorno periódicamente."""
+    respuesta = requests.get('http://127.0.0.1:5000/poll-entorno')  # Asumiendo que Flask corre en el puerto 5000 por defecto
+    print(f"Respuesta de la tarea programada: {respuesta.status_code}")
+
+sched = BackgroundScheduler(daemon=True)
+sched.add_job(tarea_programada, 'interval', seconds=POLL_TO_ENTORNO_SECONDS)
+sched.start()
+
+### Configuración de Flask ###
 application = Flask(__name__)
-# dynamodb = boto3.resource('dynamodb')
-# table = dynamodb.Table('your-table-name')
-# queue = boto3.client('sqs').get_queue_url(QueueName='your-queue-name')['QueueUrl']
 
 
+MOCK_ENTORNO_RESPUESTA = {
+    "_id": "mock_id",
+    "type": "mock_type",
+    "name": "mock_name",
+    "antsRequired": 5,
+    "timeRequired": 10,
+    "foodValue": 7
+}
+
+### Configuración de AWS DynamoDB ###
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table('your-table-name')
+
+### Endpoints de API ###
+# Este endpoint lo llama el programador (scheduler)
+# para activar el poll hacia el servicio externo "entorno".
+@application.route('/poll-entorno', methods=['GET'])
+def endpoint_poll_entorno():
+    """Endpoint para activar manualmente la encuesta."""
+    _consultar_entorno()
+    return {'status': 'consulta exitosa'}, 200
+
+def _consultar_entorno():
+    """Función para obtener datos del servicio externo entorno."""
+    application.logger.info(f"Consultando {ENTORNO_NEXT_ELEMENT_URL}")
+    # Verificar si el URL es un placeholder, y en tal caso, usar datos mock
+    if ENTORNO_NEXT_ELEMENT_URL == "http://placeholder.url/api/environment/next":
+        application.logger.info(f"Usando datos simulados para {ENTORNO_NEXT_ELEMENT_URL}")
+        application.logger.info(f"Esto escribiría en DynamoDB: {MOCK_ENTORNO_RESPUESTA}")
+        return
+
+    respuesta = requests.get(ENTORNO_NEXT_ELEMENT_URL)
+    if respuesta.status_code == 200:
+        data = respuesta.json()
+        application.logger.info(f"Dato obtenido de {ENTORNO_NEXT_ELEMENT_URL}: {data}")
+        procesar_datos_recibidos_de_entorno(data)
+    else:
+        application.logger.warning(f"No se pudo obtener datos de {ENTORNO_NEXT_ELEMENT_URL}. Estado: {respuesta.status_code}")
+
+def procesar_datos_recibidos_de_entorno(data):
+    """Función para procesar los datos recibidos y guardar en DynamoDB."""
+    tiempo_actual = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    item = {
+        'id': str(uuid.uuid4()),
+        'externalId': data.get('_id'),
+        'type': data.get('type'),
+        'name': data.get('name'),
+        'antsRequired': data.get('antsRequired'),
+        'timeRequired': data.get('timeRequired'),
+        'foodValue': data.get('foodValue', None),
+        'datetime': tiempo_actual
+    }
+    respuesta = table.put_item(Item=item)
+    application.logger.info(f"Dato guardado en DynamoDB: {item}")
+
+
+# No se usa, pero se deja como ejemplo de cómo recibir mensajes entrantes.
 @application.route('/recibir-mensaje', methods=['POST'])
-def handle_request():
+def manejar_solicitud():
+    """Endpoint para recibir y procesar mensajes entrantes."""
     data = request.get_json()
-    print('Mensaje recibido: ', data)
+    application.logger.info(f"Mensaje recibido a través de la API: {data}")
+    procesar_datos_recibidos_de_entorno(data)
+    return {'status': 'mensaje recibido con éxito'}, 200
 
-    # Store data in DynamoDB
-    ## Console log it would save here. Implement later.
-
-    # item_id = data.get('id')
-    # response = table.put_item(
-    #     Item={
-    #         'id': item_id,
-    #         'data': json.dumps(data)
-    #     }
-    # )
-
-    # Send message to SQS with delay
-    ## Console log it would send SQS message here. Implement later.
-    # response = boto3.client('sqs').send_message(
-    #     QueueUrl=queue,
-    #     MessageBody=json.dumps(data),
-    #     DelaySeconds=60
-    # )
-
-    return {'status': 'success in Beanstalk'}, 200
-
-
+### Ejecución Principal ###
 if __name__ == '__main__':
-    application.run(debug=True)
+    # Debido a que el programador (scheduler) se ejecuta en un hilo separado,
+    # tenemos que desactivar el recargado automático de Flask para evitar que se ejecuten
+    # múltiples instancias del programador.
+    application.run(debug=False, use_reloader=False)
