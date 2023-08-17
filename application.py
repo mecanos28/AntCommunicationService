cceffa6003216dfe46ba441b47d046c577708b1e
Fernando Rojas <ferdorom@amazon.com>
2023-08-16 20:50:44 -0600

    Añadiendo logica para decidir a quien enviar el mensaje

--- application.py
+++ application.py
@@ -11,6 +11,8 @@ POLL_TO_ENTORNO_SECONDS = 10  # Actualmente configurado a 10 segundos para prueb
 ENTORNO_NEXT_ELEMENT_URL = "http://ec2-52-200-81-149.compute-1.amazonaws.com/api/environment/next-task"
 HORMIGA_REQUEST_URL = "http://ec2-3-19-106-46.us-east-2.compute.amazonaws.com:38000/getHormiga" # Cambiar a la URL correcta cuando sepamos
 HORMIGA_RETURN_URL = "http://ec2-3-19-106-46.us-east-2.compute.amazonaws.com:38000/returnHormiga" # Cambiar a la URL correcta cuando sepamos
+INFORMAR_ATAQUE_URL = "http://colony-defense-service-env.eba-pmxaehhm.us-east-1.elasticbeanstalk.com:8080/swagger-ui/index.html" # Cambiar a la URL correcta cuando sepamos
+INFORMAR_COMIDA_URL = "http://colony-food-service-env.eba-2q2j2x2m.us-east-1.elasticbeanstalk.com:8080/swagger-ui/index.html" # Cambiar a la URL correcta cuando sepamos
 
 MOCK_ENTORNO_RESPUESTA_ENEMIGO = {
     "_id": "64dd6b46695b541fdf8bbbfb",
@@ -82,10 +84,28 @@ def _consultar_entorno():
 
         # añadir timer de 1 minuto
 
-        # cuando el timer llegue a 0. llamar a subsistema correspondiente. devolver hormiga. *guardar en dynamo* *estado: terminado*
+        # cuando el timer llegue a 0. llamar a subsistema correspondiente. devolver hormiga. *guardar en dynamo* *estado: terminado*\
+
+        enviar_mensaje(data, respuesta_pedir_hormiga)
 
     else:
         application.logger.warning(f"No se pudo obtener datos de {ENTORNO_NEXT_ELEMENT_URL}. Estado: {respuesta.status_code}")
+
+
+def enviar_mensaje(data, respuesta_pedir_hormiga):
+    data_a_enviar = {
+        'id': data.get('_id'),
+        'type': data.get('type'),
+        'name': data.get('name'),
+        'antsRequired': data.get('antsRequired'),
+        'timeRequired': data.get('timeRequired'),
+        'foodValue': data.get('foodValue'),
+        'id_hormiga': respuesta_pedir_hormiga.json().get('id')
+    }
+    if (data.get('type') == 'enemy'):
+        _llamar_api_informar_ataque(data)
+    else:
+        _llamar_api_informar_comida(data)
 
 
 def _llamar_api_entorno():
@@ -120,6 +140,32 @@ def _llamar_api_devolver_hormiga(id_hormiga):
     except:
         # manage respuesta could be null
         application.logger.warning(f"No se pudo obtener datos de {HORMIGA_RETURN_URL}. Estado: {respuesta.status_code}")
+
+def _llamar_api_informar_ataque(data):
+    try:
+        application.logger.info(f"Consultando {INFORMAR_ATAQUE_URL}")
+        respuesta = requests.post(INFORMAR_ATAQUE_URL, data)
+        if (respuesta.status_code == 200):
+            application.logger.info(f"Información de ataque enviada a {INFORMAR_ATAQUE_URL} con éxito data: {data}")
+            return respuesta
+        else:
+            application.logger.warning(f"No se pudo enviar información de ataque a {INFORMAR_ATAQUE_URL}. Estado: {respuesta.status_code}")
+    except:
+        # manage respuesta could be null
+        application.logger.warning(f"No se pudo obtener datos de {INFORMAR_ATAQUE_URL}. Estado: {respuesta.status_code}")
+
+def _llamar_api_informar_comida(data):
+    try:
+        application.logger.info(f"Consultando {INFORMAR_COMIDA_URL}")
+        respuesta = requests.post(INFORMAR_COMIDA_URL, data)
+        if (respuesta.status_code == 200):
+            application.logger.info(f"Información de comida enviada a {INFORMAR_COMIDA_URL} con éxito data: {data}")
+            return respuesta
+        else:
+            application.logger.warning(f"No se pudo enviar información de comida a {INFORMAR_COMIDA_URL}. Estado: {respuesta.status_code}")
+    except:
+        # manage respuesta could be null
+        application.logger.warning(f"No se pudo obtener datos de {INFORMAR_COMIDA_URL}. Estado: {respuesta.status_code}")
 
 
 def guardar_datos(data):
