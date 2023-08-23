7b0abcc8fd03c9c8b29cd045f595b0e6825b8ead
Jorge Mendez <jorgmen@gmail.com>
2023-08-22 18:21:36 -0600

    agregada funcion para actualizar estados, hormigas

--- application.py
+++ application.py
@@ -11,6 +11,7 @@ from datetime import datetime
 from apscheduler.schedulers.background import BackgroundScheduler
 from flask import Flask, request, jsonify
 from flask_restx import Api, Resource, fields
+from botocore.exceptions import ClientError
 
 ## Swagger ##
 from mocking import _mocked_response, \
@@ -20,7 +21,7 @@ SWAGGER_URL = '/api/docs'  # URL for exposing Swagger UI (without trailing '/')
 API_URL = 'static/swagger.json'  # Our API url (can of course be a local resource)
 
 ### Constantes ###
-POLL_TO_ENTORNO_SECONDS = 10  # Actualmente configurado a 10 segundos para pruebas. Debe ser 60 segundos en producción.
+POLL_TO_ENTORNO_SECONDS = 600  # Actualmente configurado a 10 segundos para pruebas. Debe ser 60 segundos en producción.
 ENTORNO_NEXT_ELEMENT_URL = "http://ec2-52-200-81-149.compute-1.amazonaws.com/api/environment/next-task"
 ENTORNO_POLLING_ACTIVE = True
 
@@ -70,6 +71,11 @@ dynamodb_record_model = api.model('Mensaje', {
 message_input_model = api.model('Data',{
     'Estado':fields.String(required=True, description='Estado del mensaje'),
     'Data': fields.String(required=True, description='El objeto mensaje convertido en string')
+})
+
+message_update_input_model = api.model('Data',{
+    'Estado':fields.String(required=True, description='Estado del mensaje'),
+    'Hormiga': fields.String(required=True, description='El id de la hormiga string')
 })
 
 
@@ -237,7 +243,14 @@ def guardar_datos(data, estado):
 def obtener_datos(id):
     """Función para obtener los datos de DynamoDB."""
     respuesta = table.get_item(Key={'Id': id})
-    return respuesta.get('Item', None)
+    item = respuesta.get('Item', None)
+    if item is None:
+        api.abort(404, "Todo {} doesn't exist".format(id))
+        return None
+    else:        
+        return item
+
+    
 
 ## Acualiza el estado de un mensaje en Dynamo DB por Id.
 def actualizar_estado(id, estado:str):
@@ -267,6 +280,24 @@ def actualizar_hormiga(id, hormiga:str):
     )
     return response
 
+### Actualiza tanto el estado como la hormiga de un mensaje ###
+def actualizar_mensaje(id, estado, hormiga):
+    try:        
+        response = table.update_item(
+            Key = { 'Id':id },
+            UpdateExpression='SET Estado = :estado_new, Hormiga = :hormiga_new',
+            ExpressionAttributeValues={
+                ':estado_new': estado,
+                ':hormiga_new': hormiga
+            },
+            ReturnValues = "UPDATED_NEW", # retorna los valores actualizados
+            ConditionExpression = 'attribute_exists(PK)',
+        )
+        return response
+    except ClientError as e:
+        api.abort(404, "Todo {} doesn't exist".format(id))
+        return None
+
 # No se usa, pero se deja como ejemplo de cómo recibir mensajes entrantes.
 @application.route('/recibir-mensaje', methods=['POST'])
 def manejar_solicitud():
@@ -283,6 +314,7 @@ class HelloWorld(Resource):
         return {'hello': 'world'}
 
 @ns.route('/<string:id>')
+@ns.response(404, 'Todo not found')
 class MensajeAPI(Resource):
     '''Muestra mensajes'''
     @ns.doc('Buscar Mensajes')
@@ -292,6 +324,13 @@ class MensajeAPI(Resource):
         msj = obtener_datos(id)
         return msj
     
+    @ns.expect(message_update_input_model)
+    @ns.marshal_with(dynamodb_record_model)
+    def put(self, id):
+        '''Actualiza Estado & Hormiga del mensaje'''
+        estado, hormiga = api.payload.values()
+        return actualizar_mensaje(id, estado, hormiga)
+    
 @ns.route('/')
 class MensajeCrear(Resource):
     @ns.expect(message_input_model)
@@ -300,7 +339,6 @@ class MensajeCrear(Resource):
         data = request.json
         res = guardar_datos(data)
         return res
-
 
 
 
